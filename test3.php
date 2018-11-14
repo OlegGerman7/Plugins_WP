0dfb91aa6d63c064463e553013f674eb2195c967
OlegGerman7 <oleg.german7@gmail.com>
2018-11-14 12:06:41 +0200

    commit

---
+++ test3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+	
+	<form method="POST" action="test_form.php">
+		<input type="text" name="text3">
+		<input type="submit" name="sub" value="Send">
+	</form>
+	
+</body>
+</html>
